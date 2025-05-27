69585890ff89b6d000e64b1f3ffad3e40e93bd7e
nimaaskarian <nimaaskarian.work@outlook.com>
2025-05-27 17:43:14 +0330

    Fix marshalize of event

--- aw-core.go
+++ aw-core.go
@@ -1,12 +1,28 @@
 package aw_go
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type Id int
 
+type IsoTime time.Time
+type SecondsDuration time.Duration
+
+func (itime IsoTime) MarshalJSON() ([]byte, error) {
+	t := time.Time(itime)
+	return json.Marshal(t.Format(time.RFC3339))
+}
+
+func (sec_duration SecondsDuration) MarshalJSON() ([]byte, error) {
+	duration := time.Duration(sec_duration)
+	return json.Marshal(uint(duration.Seconds()))
+}
+
 type Event struct {
-	Id        *Id
-	Timestamp time.Time
-	Duration  time.Duration
-	Data      map[string]interface{}
+	Id        *Id                    `json:"id"`
+	Timestamp IsoTime                `json:"timestamp"`
+	Duration  SecondsDuration        `json:"duration"`
+	Data      map[string]interface{} `data:"duration"`
 }
